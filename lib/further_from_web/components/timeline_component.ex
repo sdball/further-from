ed8a43778ecb38889645d7e2470a1257968901cf
Stephen Ball <sdball@gmail.com>
2022-12-18 08:40:23 -0500

    Add and use TimelineComponent.comparison_summary

--- lib/further_from_web/components/timeline_component.ex
+++ lib/further_from_web/components/timeline_component.ex
@@ -13,4 +13,42 @@ defmodule FurtherFromWeb.TimelineComponent do
     </div>
     """
   end
+
+  def comparison_summary(assigns) do
+    cond do
+      assigns.comparison.pivot_year < assigns.current_year ->
+        past_comparison(assigns)
+
+      true ->
+        future_comparison(assigns)
+    end
+  end
+
+  def past_comparison(assigns) do
+    ~H"""
+    <div class="flex-start flex items-center pt-3">
+      <div class="-ml-1 mr-3 h-2 w-2 rounded-full bg-gray-300"></div>
+      <p class="text-2xl text-gray-800"><%= @comparison.pivot_year %></p>
+    </div>
+    <div class="mt-0.5 ml-4 mb-6">
+      <h4 class="mb-1.5 text-xl font-semibold text-gray-800">
+        We got further from the <%= @comparison.last.short_name %> than the <%= @comparison.last.short_name %> was from the <%= @comparison.first.short_name %>.
+      </h4>
+    </div>
+    """
+  end
+
+  def future_comparison(assigns) do
+    ~H"""
+    <div class="flex-start flex items-center pt-3">
+      <div class="-ml-1 mr-3 h-2 w-2 rounded-full bg-gray-300"></div>
+      <p class="text-2xl text-gray-800"><%= @comparison.pivot_year %></p>
+    </div>
+    <div class="mt-0.5 ml-4 mb-6">
+      <h4 class="mb-1.5 text-xl font-semibold text-gray-800">
+        In <%= @comparison.pivot_year %> we'll be further from the <%= @comparison.last.short_name %> than the <%= @comparison.last.short_name %> was from the <%= @comparison.first.short_name %>. These events were <%= @comparison.difference %> years apart.
+      </h4>
+    </div>
+    """
+  end
 end
