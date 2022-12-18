675b1ee7f92555c750169841a9ba59d6fcc42198
Stephen Ball <sdball@gmail.com>
2022-12-18 11:24:38 -0500

    Use live navigation

--- lib/further_from_web/live/comparison_live.ex
+++ lib/further_from_web/live/comparison_live.ex
@@ -3,12 +3,16 @@ defmodule FurtherFromWeb.ComparisonLive do
   alias FurtherFromWeb.TimelineComponent
   require Logger
 
-  def mount(%{"event1" => event1_key, "event2" => event2_key}, _session, socket) do
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def handle_params(%{"event1" => event1_key, "event2" => event2_key}, _url, socket) do
     current_year = Date.utc_today().year
     event1 = get_event(event1_key)
     event2 = get_event(event2_key)
     comparison = FurtherFrom.Engine.compare(event1, event2)
-    {:ok, assign(socket, comparison: comparison, current_year: current_year)}
+    {:noreply, assign(socket, comparison: comparison, current_year: current_year)}
   end
 
   defp get_event(key) do
